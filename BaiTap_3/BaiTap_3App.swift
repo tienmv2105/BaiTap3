9d9803fa0f112dd4277e9dc85d4306e67d72b69d
BIN <maivantien210503@gmail.com>
2026-06-13 10:55:32 +0700

    init

---
+++ BaiTap_3/BaiTap_3App.swift
@@ -0,0 +1,17 @@
+//
+//  BaiTap_3App.swift
+//  BaiTap_3
+//
+//  Created by Van Tien on 13/6/26.
+//
+
+import SwiftUI
+
+@main
+struct BaiTap_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BaiTap_3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BaiTap_3
+//
+//  Created by Van Tien on 13/6/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
